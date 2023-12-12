65a28d838a0692a19026278aa847f885257e4ee4
appdevtk <appdevtk@gmail.com>
2023-12-12 16:25:10 +0000

    Revert "Enable full rgb and settings"
    This reverts commit 9ecdd171bc9e16633b8fbb879c932a0b2ccf9a2d.

--- keyboards/splitkb/aurora/lily58/rev1/keymaps/vial/rules.mk
+++ keyboards/splitkb/aurora/lily58/rev1/keymaps/vial/rules.mk
@@ -4,14 +4,6 @@ ENCODER_MAP_ENABLE = yes
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 
-# My setup
-OLED_ENABLE = yes
-COMBO_ENABLE = yes
-KEY_OVERRIDE_ENABLE = yes
-RGB_MATRIX_ENABLE = yes
-RGBLIGHT_ENABLE = yes
-ENCODER_ENABLE = yes
-
 # Saving space on atmega32u4
 ifeq ($(strip $(MCU)), atmega32u4)
     OLED_ENABLE = no
