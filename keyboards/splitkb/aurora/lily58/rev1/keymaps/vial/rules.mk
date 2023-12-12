2ecedb9ea7131aa9398db3b09559cd3303166522
appdevtk <appdevtk@gmail.com>
2023-12-12 17:47:03 +0000

    Next RGB try

--- keyboards/splitkb/aurora/lily58/rev1/keymaps/vial/rules.mk
+++ keyboards/splitkb/aurora/lily58/rev1/keymaps/vial/rules.mk
@@ -5,19 +5,13 @@ VIA_ENABLE = yes
 VIAL_ENABLE = yes
 
 # RGB
+RGB_MATRIX_ENABLE = yes
+RGBLIGHT_ENABLE = no
 VIALRGB_ENABLE = yes
 
 # OLED
 OLED_ENABLE = yes
 OLED_DRIVER = ssd1306
-
-# Saving space on atmega32u4
-ifeq ($(strip $(MCU)), atmega32u4)
-    OLED_ENABLE = no
-    MOUSEKEY_ENABLE = no
-    COMBO_ENABLE = no
-    KEY_OVERRIDE_ENABLE = no
-endif
 
 ## build targets
 # Liatris
